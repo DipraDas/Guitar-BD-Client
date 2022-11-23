000e01392b8e1f60449de76f79eccdd801fad205
Dipra Das <dipradas5940@gmail.com>
2022-11-24 00:49:17 +0600

    Firebase Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <button className="btn btn-primary">Button</button>
+      <button className="btn btn-primary">Button</button>
     </div>
   );
 }
