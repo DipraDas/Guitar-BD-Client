348744376d7556080d122ba9c1d7f07bdd691779
Dipra Das <dipradas5940@gmail.com>
2022-11-24 11:49:45 +0600

    Navbar Setup

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <button className="btn btn-primary">Button</button>
+            Home
         </div>
     );
 };
